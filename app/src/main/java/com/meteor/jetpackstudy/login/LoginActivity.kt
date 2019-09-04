071f092007c88ad0decc51d4b0a2f783cbc9fe75
X_Meteor <lx802315@163.com>
2019-09-04 13:41:41 +0800

    navigation 相关完成

---
+++ app/src/main/java/com/meteor/jetpackstudy/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.meteor.jetpackstudy.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.meteor.jetpackstudy.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/meteor/jetpackstudy/login/RegisterFragment.kt
@@ -0,0 +1,48 @@
+package com.meteor.jetpackstudy.login
+
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.meteor.jetpackstudy.MainActivity
+
+import com.meteor.jetpackstudy.R
+import kotlinx.android.synthetic.main.fragment_register.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class RegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        txt_cancel.setOnClickListener {
+            activity?.onBackPressed()
+        }
+
+        btn_register.setOnClickListener {
+            startActivity(Intent(activity, MainActivity::class.java))
+            activity?.finish()
+        }
+
+        val safeArgs: RegisterFragmentArgs by navArgs()
+        val email = safeArgs.email
+        et_email.setText(email)
+
+    }
+
+
+}
